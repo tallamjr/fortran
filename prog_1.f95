7a6ed7238f70bd864e478a29152331345a788485
Tarek Allam <t.allam.jr@gmail.com>
2015-11-18 15:47:16 +0000

    First 2 programs

--- prog_1.f95
+++ prog_1.f95
@@ -1,3 +1,3 @@
-    PRINT *,'Hello World !'
+    PRINT *,'Hello World !', Pi=' ,4.*atan(1.)
     END
 
---
+++ prog_2.f95
@@ -0,0 +1,8 @@
+   10 PRINT *,'Enter a TEmp. in Cent. (<-273.15 to stop)'
+      READ *,Cent
+      IF(Cent.LT.-273.15)STOP
+      Fahr=32+1.8*Cent
+      PRINT *,'A' ,Cent, '     Centigrade temperature is ',
+     + Fahr,' Fahrenheit'
+      GO TO 10
+      END
