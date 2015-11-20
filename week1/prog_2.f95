839b9b3ec7dad71d9579d64a9ffef9c1d287f2bf
Tarek Allam <t.allam.jr@gmail.com>
2015-11-20 00:59:45 +0000

    Executables for prog 2 and 3, higher precisions of PI

--- week1/prog_2.f95
+++ week1/prog_2.f95
@@ -1,8 +1,7 @@
-   10 PRINT *,'Enter a TEmp. in Cent. (<-273.15 to stop)'
+   10 PRINT *,'Enter a Temp. in Cent. (<-273.15 to stop)'
       READ *,Cent
       IF(Cent.LT.-273.15)STOP
       Fahr=32+1.8*Cent
-      PRINT *,'A' ,Cent, '     Centigrade temperature is ',
-     + Fahr,' Fahrenheit'
+      PRINT *,'A' ,Cent, '     Centigrade temperature is ', Fahr,' Fahrenheit'
       GO TO 10
       END
